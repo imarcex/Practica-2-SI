cd550666ccb66c99c8175534231304857707c226
test <72299419+test@users.noreply.github.com>
2024-04-28 23:02:18 +0200

    Update ejercicio2.py

--- src/utils/ejercicio2.py
+++ src/utils/ejercicio2.py
@@ -2,7 +2,7 @@ from utils.internal_interfaces import __get_all_critical_users
 
 def get_critical_users_clicked_spam(sampleLength: int, above_fifty_percent: bool):
     critical_users = __get_all_critical_users()
-    critical_users['perone'] = critical_users['cliclados'] / critical_users['total']
+    critical_users['perone'] = critical_users['cliclados'] / critical_users['phishing']
 
     if above_fifty_percent:
         criba = critical_users[critical_users['perone'] > 0.5]

d598f0e0def3ae3f8d989b2004380d94596ea0b3
imarcex <imarcex@zeroclick.sh>
2024-04-28 23:01:24 +0200

    ejercicio 2 working

--- src/utils/ejercicio2.py
+++ src/utils/ejercicio2.py
@@ -2,16 +2,13 @@ from utils.internal_interfaces import __get_all_critical_users
 
 def get_critical_users_clicked_spam(sampleLength: int, above_fifty_percent: bool):
     critical_users = __get_all_critical_users()
-    print(critical_users)
     critical_users['perone'] = critical_users['cliclados'] / critical_users['total']
 
     if above_fifty_percent:
         criba = critical_users[critical_users['perone'] > 0.5]
     else:
         criba = critical_users[critical_users['perone'] < 0.5]
-    
-    criba['percent'] = str(criba['perone']* 100) + '%'
-    
-    usuario_percent = criba[["username", "percent"]][:sampleLength]
+     
+    usuario_percent = criba[["username", "perone"]][:sampleLength]
     
     return list(usuario_percent.itertuples(index=False, name=None))
